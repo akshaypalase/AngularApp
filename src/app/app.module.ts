e77cbfb4f11b13efe925094e5e06b9dc27aa22ef
akshaypalase <akshaypalase3@gmail.com>
2022-11-24 13:43:18 +0530

    updated
    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { DatabindingAssignmentComponent } from './databinding-assignment/databinding-assignment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    DatabindingAssignmentComponent
   ],
   imports: [
     BrowserModule,
