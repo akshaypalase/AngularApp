543b7d93ded3147fb031ad084a83c70eeef91d2b
akshaypalase <akshaypalase3@gmail.com>
2022-12-01 23:00:27 +0530

    updated
    updated

--- src/app/structuraldirective/structuraldirective.component.ts
+++ src/app/structuraldirective/structuraldirective.component.ts
@@ -21,9 +21,6 @@ export class StructuraldirectiveComponent implements OnInit {
     {CourseName:"Electrical",Duration:"4",CourseFee:"70000",CourseType:"Offline"},
     {CourseName:"Civil",Duration:"4",CourseFee:"80000 ",CourseType:"Offline"},
     {CourseName:"Mechanical",Duration:" 4",CourseFee:"65000",CourseType:"Offline"},
-    {CourseName:"Electronics & Telecommunication",Duration:"3",CourseFee:"67000 ",CourseType:"Offline"},
-    {CourseName:"Chemical",Duration:"3.5",CourseFee:"60000 ",CourseType:"Offline"},
-    {CourseName:"Mechatronics",Duration:"2.5",CourseFee:"55000 ",CourseType:"Offline"}
   ]
     selectedPro : string;
   getProductVal(value : any){
